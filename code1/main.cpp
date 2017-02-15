78f6edde01f462fbb8a17cbd68a901fa2b1c5ec6
Christian Ondaatje <christian.ondaatje@gmail.com>
2017-02-14 19:46:28 -0500

    Initial Commit

---
+++ code1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  code1
+//
+//  Created by Eagle on 2/14/17.
+//  Copyright © 2017 Ondaatje124. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
